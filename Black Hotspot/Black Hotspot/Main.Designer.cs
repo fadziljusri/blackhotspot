35f0883537d375e65cfc2ed7507119badf4a6af1
fadziljusri <fadziljusri@gmail.com>
2016-10-27 12:10:16 +0800

    zzz

---
+++ Black Hotspot/Black Hotspot/Main.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Black_Hotspot
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Start_Stop = new System.Windows.Forms.Button();
+            this.hname = new System.Windows.Forms.TextBox();
+            this.pwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hint = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Start_Stop
+            // 
+            this.Start_Stop.Location = new System.Drawing.Point(33, 169);
+            this.Start_Stop.Name = "Start_Stop";
+            this.Start_Stop.Size = new System.Drawing.Size(305, 35);
+            this.Start_Stop.TabIndex = 0;
+            this.Start_Stop.Text = "Start";
+            this.Start_Stop.UseVisualStyleBackColor = true;
+            this.Start_Stop.Click += new System.EventHandler(this.Start_Stop_Click);
+            // 
+            // hname
+            // 
+            this.hname.Location = new System.Drawing.Point(102, 29);
+            this.hname.Name = "hname";
+            this.hname.Size = new System.Drawing.Size(168, 20);
+            this.hname.TabIndex = 1;
+            // 
+            // pwd
+            // 
+            this.pwd.Location = new System.Drawing.Point(102, 64);
+            this.pwd.Name = "pwd";
+            this.pwd.Size = new System.Drawing.Size(168, 20);
+            this.pwd.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hotspot Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // hint
+            // 
+            this.hint.AutoSize = true;
+            this.hint.Location = new System.Drawing.Point(102, 90);
+            this.hint.Name = "hint";
+            this.hint.Size = new System.Drawing.Size(53, 17);
+            this.hint.TabIndex = 5;
+            this.hint.Text = "Show";
+            this.hint.UseVisualStyleBackColor = true;
+            this.hint.CheckedChanged += new System.EventHandler(this.hint_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.hname);
+            this.groupBox1.Controls.Add(this.hint);
+            this.groupBox1.Controls.Add(this.pwd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(33, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 124);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setup";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 221);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Start_Stop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Black Hotspot";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_Stop;
+        private System.Windows.Forms.TextBox hname;
+        private System.Windows.Forms.TextBox pwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox hint;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Black Hotspot/Black Hotspot/Main.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.IO;
+
+namespace Black_Hotspot
+{
+    public partial class Main : Form
+    {
+        string path;
+        bool connect = false;
+
+        public Main()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            pwd.PasswordChar = '#';
+
+            string curr = Directory.GetCurrentDirectory();
+            path = curr + "\\conf.txt";
+            //MessageBox.Show(path, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                StreamWriter sw = File.CreateText(path);
+            }
+            else
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    s = sr.ReadLine();
+                    hname.Text = s;
+                    s = sr.ReadLine();
+                    pwd.Text = s;
+                }
+            }
+        }
+
+        public static bool IsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public void Start()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.CreateNoWindow = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = System.Windows.Forms.Application.ExecutablePath;
+            startInfo.Verb = "runas";
+            try
+            {
+                Process p = Process.Start(startInfo);
+            }
+            catch
+            {
+
+            }
+
+            System.Windows.Forms.Application.Exit();
+        }
+
+
+        private void Start_Stop_Click(object sender, EventArgs e)
+        {
+            string ssid = hname.Text, key = pwd.Text;
+
+            if (!connect)
+            {
+                if (String.IsNullOrEmpty(hname.Text))
+                {
+                    MessageBox.Show("Hotspot Name cannot be left blank !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    if (pwd.Text == null || pwd.Text == "")
+                    {
+                        MessageBox.Show("Password cannot be left blank !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        if (key.Length < 8)
+                        {
+                            MessageBox.Show("Password should be more than or equal to 8 !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            Hotspot(ssid, key, true);
+                            hname.Enabled = false;
+                            pwd.Enabled = false;
+                            Start_Stop.Text = "Stop";
+                            connect = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Hotspot(null, null, false);
+                hname.Enabled = true;
+                pwd.Enabled = true;
+                Start_Stop.Text = "Start";
+                connect = false;
+            }
+        }
+
+        private void Hotspot(string ssid, string key, bool status)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe");
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            Process process = Process.Start(processStartInfo);
+
+            if (process != null)
+            {
+                if (status)
+                {
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine(ssid);
+                        sw.Write(key);
+                    }
+                    process.StandardInput.WriteLine("netsh wlan set hostednetwork mode=allow ssid=" + ssid + " key=" + key);
+                    process.StandardInput.WriteLine("netsh wlan start hosted network");
+                    process.StandardInput.Close();
+                }
+                else
+                {
+                    process.StandardInput.WriteLine("netsh wlan stop hostednetwork");
+                    process.StandardInput.Close();
+                }
+            }
+        }
+
+        private void hint_CheckedChanged(object sender, EventArgs e)
+        {
+            if (hint.Checked == false)
+            {
+                hint.Text = "Show";
+                pwd.PasswordChar = '#';
+            }
+            else
+            {
+                hint.Text = "Hide";
+                pwd.PasswordChar = '\0';
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            if (!IsAdmin())
+            {
+                Start();
+            }
+        }
+
+        //private void Main_Closed(object sender, EventArgs e)
+        //{
+            //MessageBox.Show("aaaa");
+            //Hotspot(null, null, false);
+            //Application.Exit();
+        //}
+    }
+}
